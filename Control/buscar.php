eca8f18ad106c979e66f57a00955d9bcee82d45b
ivants <ivanfcc2@outlook.com>
2017-03-23 17:54:54 -0600

    Conexion a la base de datos Busqueda en tiempo real desde el input de busqueda ERROR EN LA CONEXION

---
+++ Control/buscar.php
@@ -0,0 +1,27 @@
+<?php
+	//Archivo de conexión a la base de datos
+	require('login_db.php');
+
+	//Variable de búsqueda
+	$consultaBusqueda = $_POST['busqueda'];
+
+	//Filtro anti-XSS
+	$caracteres_malos = array("<", ">", "\"", "'", "/", "<", ">", "'", "/");
+	$caracteres_buenos = array("& lt;", "& gt;", "& quot;", "& #x27;", "& #x2F;", "& #060;", "& #062;", "& #039;", "& #047;");
+	$consultaBusqueda = str_replace($caracteres_malos, $caracteres_buenos, $consultaBusqueda);
+
+	//Variable vacía (para evitar los E_NOTICE)
+	$mensaje = "";
+
+	//Comprueba si $consultaBusqueda está seteado
+	if (isset($consultaBusqueda)) {
+
+		//Selecciona todo de la tabla mmv001 
+		//donde el nombre sea igual a $consultaBusqueda, 
+		//o el apellido sea igual a $consultaBusqueda, 
+		//o $consultaBusqueda sea igual a nombre + (espacio) + apellido
+		$consulta = mysqli_query($con, "SELECT * FROM PRODUCTOS
+		WHERE CATEGORIA,CODIGO,DESCRIPCION,MARCA COLLATE UTF8_SPANISH_CI LIKE '%$consultaBusqueda%'");
+
+	};
+?>
