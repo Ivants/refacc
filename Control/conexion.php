b7f1dd0a46848cfe4614dca7f54d73d4d343827d
ivants <ivanfcc2@outlook.com>
2017-03-23 13:31:10 -0600

    Creacion del WebService Modificacion del maquetado del website Modificacion de la base de datos de prueba en la que se hace uso de proveedores y productos Se agregaron las primeras instancias para la conexion del webservice La base de datos esta en linea en una cuenta de prueba en hostinguer

---
+++ Control/conexion.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Proveedor de las constantes para conectarse a la base de datos Mysql.
+ */
+define("HOSTNAME", "mysql.hostinger.mx");	//host
+define("DATABASE", "u929355182_refac"); 		//base de datos
+define("USERNAME", "u929355182_refac"); 		//usuario
+define("PASSWORD", "refac123"); 			//contraseña
+?>
